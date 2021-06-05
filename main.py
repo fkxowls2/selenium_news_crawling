921560698b446c9831618e296936be1d3004702e
racoding <fkxowls@gmail.com>
2021-06-05 19:00:37 +0900

    add except

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ browser.get("https://search.naver.com/search.naver?query="+keyword+"&where=news&
 # 페이지별 언론사,기사제목,링크 수집하여 txt로 저장
 file = open("d:\\data\\naver_news.txt","w")   # 텍스트 파일을 저장할 경로
 for i in range(1, page+1):
-    browser.find_element_by_link_text(str(i)).click()   # 링크가 있는 태그의 텍스트로 찾아서 페이지 클릭
-    time.sleep(3)
-    press = browser.find_elements_by_css_selector('a.press')   # 태그와 클래스로 추출
-    title = browser.find_elements_by_css_selector('a.news_tit')
-    for i, j in zip(press,title):
-        file.write(i.text+'\t'+j.text+'\t'+j.get_attribute('href')+'\n')   # 텍스트 파일에 저장
+    try:
+        browser.find_element_by_link_text(str(i)).click()   # 링크가 있는 태그의 텍스트로 찾아서 페이지 클릭
+        time.sleep(3)
+        press = browser.find_elements_by_css_selector('a.press')   # 태그와 클래스로 추출
+        title = browser.find_elements_by_css_selector('a.news_tit')
+        for i, j in zip(press,title):
+            file.write(i.text+'\t'+j.text+'\t'+j.get_attribute('href')+'\n')   # 텍스트 파일에 저장
+    except:
+        break
 file.close()
 
 # 브라우저 종료
